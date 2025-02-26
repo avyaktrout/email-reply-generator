5c8ec018e73e514bb7e854c565e1b96d42a3391d
avyaktrout <avyakt.rout@outlook.com>
2025-02-25 22:39:06 -0800

    Initial commit

---
+++ email-writer-sb/src/main/java/com/email/writer/EmailWriterSbApplication.java
@@ -0,0 +1,14 @@
+package com.email.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.email.writer", "app"})
+public class EmailWriterSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriterSbApplication.class, args);
+	}
+}
